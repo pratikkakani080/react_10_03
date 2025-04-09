1446db1e8b02f30f80e66bf219886a6073288983
Pratik <pratik080.rejoice@gmail.com>
2025-04-09 16:59:12 +0530

    feat: form validation

--- src/module/contactus/index.jsx
+++ src/module/contactus/index.jsx
@@ -1,47 +1,132 @@
 import React, { useState } from "react";
 
 function ContactUs() {
-  const [user, setUser] = useState({})
-  console.log(user);
-  
-  // const handleChange = (e) => {
-  //   // console.log(e);
-  //   setFname(e.target.value)
-  // }
+  const [user, setUser] = useState({});
+  const [error, setError] = useState({});
+  console.log("error", error);
+
+  const handleChange = (ev) => {
+    // setUser((prev) => {
+    //   return { ...prev, [ev.target.name]: ev.target.value };
+    // })
+    const { name, value } = ev.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const isValidate = () => {
+    let formIsValid = true;
+    let errors = {};
+    if (!user.fname) {
+      formIsValid = false;
+      errors.fname = "please enter valid first name";
+    }
+    if (!user.lname) {
+      formIsValid = false;
+      errors.lname = "please enter valid last name";
+    }
+    if (!user.email) {
+      formIsValid = false;
+      errors.email = "please enter valid email";
+    }
+    if (!user.password) {
+      formIsValid = false;
+      errors.password = "please enter valid password";
+    }
+    if (!user.confirmPassword) {
+      formIsValid = false;
+      errors.confirmPassword = "please enter valid confirm password";
+    }
+    setError(errors);
+    return formIsValid;
+  };
 
   const handleSubmit = () => {
-    // localStorage.setItem('fname', fname)
-    // localStorage.setItem('lname', lname)
-    localStorage.setItem('user', JSON.stringify(user))
-  }
+    if (isValidate()) {
+      localStorage.setItem("user", JSON.stringify(user));
+    }
+  };
 
   return (
     <div>
-      <label htmlFor="fname">First name:</label>
+      <label htmlFor="fname">
+        First name:
+        {error.fname && (
+          <span style={{ color: "red", fontSize: "10px" }}>
+            * {error.fname}
+          </span>
+        )}
+      </label>
       <br />
-      <input type="text" id="fname" name="fname"  onChange={(ev) => {setUser(ev.target.value); 
-      // console.log(ev, ev.target.name, ev.target.value);
-      }} />
+      <input
+        type="text"
+        id="fname"
+        name="fname"
+        onChange={(ev) => handleChange(ev)}
+      />
       <br />
-      <label htmlFor="lname">Last name:</label>
+      <label htmlFor="lname">
+        Last name:{" "}
+        {error.lname && (
+          <span style={{ color: "red", fontSize: "10px" }}>
+            * {error.lname}
+          </span>
+        )}
+      </label>
       <br />
-      <input type="text" id="lname" name="lname" onChange={(ev => {setUser(ev.target.value); 
-      // console.log(ev, ev.target.name, ev.target.value);
-      })} />
+      <input
+        type="text"
+        id="lname"
+        name="lname"
+        onChange={(ev) => handleChange(ev)}
+      />
       <br />
-      <label htmlFor="email">Email:</label>
+      <label htmlFor="email">
+        Email:{" "}
+        {error.email && (
+          <span style={{ color: "red", fontSize: "10px" }}>
+            * {error.email}
+          </span>
+        )}
+      </label>
       <br />
-      <input type="email" id="email" name="email" onChange={(ev => {setUser(ev.target.value);
-      //  console.log(ev, ev.target.name, ev.target.value);
-      })} />
+      <input
+        type="email"
+        id="email"
+        name="email"
+        onChange={(ev) => handleChange(ev)}
+      />
       <br />
-      <label htmlFor="password">Password:</label>
+      <label htmlFor="password">
+        Password:{" "}
+        {error.password && (
+          <span style={{ color: "red", fontSize: "10px" }}>
+            * {error.password}
+          </span>
+        )}
+      </label>
       <br />
-      <input type="password" id="password" name="password" onChange={(ev => setUser(ev.target.value))} />
+      <input
+        type="password"
+        id="password"
+        name="password"
+        onChange={(ev) => handleChange(ev)}
+      />
       <br />
-      <label htmlFor="confirmPassword">Confirm Password:</label>
+      <label htmlFor="confirmPassword">
+        Confirm Password:{" "}
+        {error.confirmPassword && (
+          <span style={{ color: "red", fontSize: "10px" }}>
+            * {error.confirmPassword}
+          </span>
+        )}
+      </label>
       <br />
-      <input type="password" id="confirmPassword" name="confirmPassword" onChange={(ev => setUser(ev.target.value))} />
+      <input
+        type="password"
+        id="confirmPassword"
+        name="confirmPassword"
+        onChange={(ev) => handleChange(ev)}
+      />
       <br />
       <br />
       <input type="submit" value="Submit" onClick={handleSubmit} />
